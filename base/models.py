a4d9ed7dc600e57add8e06a425828c658298998c
Manny Ezagwula <laphixer@gmail.com>
2023-08-06 22:19:12 +0100

    Added Recent Activity sidebar with activity feeds, added filter to the 'Browse topic' column and created reusuable components

--- base/models.py
+++ base/models.py
@@ -37,5 +37,11 @@ class Message(models.Model):
    update = models.DateTimeField(auto_now=True)
    created = models.DateTimeField(auto_now_add=True)
 
+   class Meta:
+       ordering =['-created', '-update']
+
+   
+
+
    def __str__(self):
        return self.body[0:50]
