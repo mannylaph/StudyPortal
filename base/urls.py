5cee3a002cd1bf6c5090171637d065aa4fa8a3ba
Manny Ezagwula <laphixer@gmail.com>
2023-08-07 12:32:44 +0100

    Added user profile with activity feeds along with links to user profile

--- base/urls.py
+++ base/urls.py
@@ -11,6 +11,7 @@ urlpatterns =[
     path('',views.home, name= "Homepage"),
     path('room/<str:pk>/', views.room, name="the_room"),
     path('chats/', views.chats, name= "chat_room"),
+    path('profile/<str:pk>/', views.userProfile, name= 'user-profile'),
 
     path('create-room/',views.createRoom, name ="create-room"),
     path('update-room/<str:pk>',views.updateRoom, name ="update-room"),
--- base/views.py
+++ base/views.py
@@ -126,6 +126,18 @@ def room(request,pk):
 def chats(request):
     return render(request,'base/chats.html')
 
+#logic for user profile
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+
+    room_count = rooms.count()
+    context ={'user':user, 'rooms':rooms, 'room_count':room_count, 'room_messages':room_messages, 'topics':topics}
+    return render(request, 'base/profile.html',context)
+
+
 
 # Logic for creating new room
 @login_required(login_url ='/login') # Restricts this function to authenicated users only
