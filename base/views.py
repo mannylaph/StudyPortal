73b5136f9b72648bd8511a240615aa6c0ec621d8
Manny Ezagwula <laphixer@gmail.com>
2023-08-07 14:09:54 +0100

    Added static folder, images and style folder. Added a main.css file

--- base/views.py
+++ base/views.py
@@ -146,8 +146,10 @@ def createRoom(request):
     if request.method == "POST":
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect("Homepage")
+           room = form.save(commit=False)
+           room.host = request.user
+           room.save()
+        return redirect("Homepage")
     context = {'form':form}
     return render(request, 'base/room_form.html', context)
 
