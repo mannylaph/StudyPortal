dde0c965871b6516505fe2a964efcf9ff6c506f0
Manny Ezagwula <laphixer@gmail.com>
2023-08-11 00:19:04 +0100

    Set up activity page for mobile layout

--- base/views.py
+++ base/views.py
@@ -87,7 +87,7 @@ def home(request):
             Q(description__icontains =q)
     )
     
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages =Message.objects.filter(Q(room__name__icontains=q))
 
@@ -231,3 +231,18 @@ def updateUser(request):
             return redirect('user-profile', pk=user.id)
 
     return render(request,'base/update-user.html',{'form':form})
+
+
+
+#logic for topics
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request,'base/topics.html',{'topics':topics})
+
+
+#logic for Activities
+def activityPage(request):
+    room_messages = Message.objects.all()
+    return render(request, 'base/activity.html',{'room_messages':room_messages})
+
