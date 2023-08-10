b6b86e9abc848b4bc91ad8bbdd00f3958419ee15
Manny Ezagwula <laphixer@gmail.com>
2023-08-10 13:18:04 +0100

    Added user registration custom page and form

--- base/forms.py
+++ base/forms.py
@@ -1,9 +1,21 @@
 from django.forms import ModelForm
 from base.models import Room
+from django.contrib.auth.models import User
 
+
+#logic for Room Form
 class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = '__all__'
         exclude = ['host','participants']
+
+
+
+#logic for User Form
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username','email']
+        
     
