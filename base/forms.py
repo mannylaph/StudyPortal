4b233dbc8e5de38ca291dcbbd560f0102fea0aaa
Manny Ezagwula <laphixer@gmail.com>
2023-08-14 10:16:27 +0100

    Customized the Django model, user forms , added user avatar and email for login

--- base/forms.py
+++ base/forms.py
@@ -1,6 +1,13 @@
 from django.forms import ModelForm
-from base.models import Room
-from django.contrib.auth.models import User
+from base.models import Room, User
+from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.models import User
+
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['name','username','email','password1','password2']
 
 
 #logic for Room Form
@@ -16,6 +23,6 @@ class RoomForm(ModelForm):
 class UserForm(ModelForm):
     class Meta:
         model = User
-        fields = ['username','email']
+        fields = ['avatar','name','username','email','bio']
         
     
